d7fe5d3ae819ee1077794df13ea8276c90c29b79
Vasily Filin <pro.vasiliy.filin@gmail.com>
2024-03-06 18:20:32 +0300

    AlgorithmsHWp1 init

---
+++ src/main/java/skypro/algorithmshwp1/AlgorithmsHwP1Application.java
@@ -0,0 +1,13 @@
+package skypro.algorithmshwp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmsHwP1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgorithmsHwP1Application.class, args);
+	}
+
+}
---
+++ src/test/java/skypro/algorithmshwp1/AlgorithmsHwP1ApplicationTests.java
@@ -0,0 +1,13 @@
+package skypro.algorithmshwp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmsHwP1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
